6fa5267935e7b388b24ba2d3b6b1825cf25d6fb8
Kaos <91225055+SoyJuaneto@users.noreply.github.com>
2024-05-09 12:16:03 -0500

    Listado de objetos

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const fs = require("fs");
 
+//const dishes = {};
+let dishes;
 
+fs.readFile("resources/dishes.json", (err, data) => {
+  if (!err) {
+    console.log("Platos leidos correcatamente");
+    dishes = JSON.parse(data);
+  }
+});
 
-router.get('/', (req, res) => {
-    res.render('index.ejs',{
-        title: 'Taller #1'
-        
-    })
-})
+router.get("/", (req, res) => {
+  res.render("index.ejs", {
+    title: "Taller #1",
+    dishes: dishes,
+  });
+});
 
-module.exports = router
+module.exports = router;
