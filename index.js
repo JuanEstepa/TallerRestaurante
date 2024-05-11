02fb57f352b4a4a00e8aff8e7f575b25304e3eff
Kaos <91225055+SoyJuaneto@users.noreply.github.com>
2024-05-11 17:26:54 -0500

    Doc and grid menú

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
 //SETTERS
 
-app.set('views','./views')
-app.set('view engine','ejs')
-app.set('PORT', process.env.PORT || 3000)
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.set("PORT", process.env.PORT || 3000);
 
 //MIDDLEWARE
 
-app.use("/",require('./routes/index'))
+//utiliza ra ruta raiz / para ejecutar el index
+app.use("/", require("./routes/index"));
 
-app.listen(app.get('PORT'),()=>console.log(`Server is listen at port: ${app.get('PORT')}`))
+//ejecuta el puerto donde se utilizara el servidor
+app.listen(app.get("PORT"), () =>
+  console.log(`Server is listen at port: ${app.get("PORT")}`)
+);
--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,22 @@ const fs = require("fs");
 //const dishes = {};
 let dishes;
 
+//SE UTILIZA LA FUNCION FS DE NODE PARA LEER UN JSON
 fs.readFile("resources/dishes.json", (err, data) => {
   if (!err) {
     console.log("Platos leidos correcatamente");
+
+    //SE ALMACENA EL JSON PARSEADO EN LA VARIABLE DISHES
     dishes = JSON.parse(data);
   }
 });
 
+//CONSULTA DE TODOS LOS PLATOS QUE ESTAN EN EL MENÚ
 router.get("/", (req, res) => {
   res.render("index.ejs", {
     title: "Taller #1",
+
+    //SE ENVIA EL JSON EN UNA VARIABLE DISHES
     dishes: dishes,
   });
 });
